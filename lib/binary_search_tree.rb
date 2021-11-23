051447286eeaf33856083b5abb0d5d1e52eacb03
Kenneth Fitzgerald <fitzgeraldkd@gmail.com>
2021-11-22 23:44:25 -0500

    commit tree 01d0dd640bbefdc29762ebc68eed74383481bae3

--- lib/binary_search_tree.rb
+++ lib/binary_search_tree.rb
@@ -8,11 +8,35 @@ class BinarySearchTree
   end
 
   def search(value)
-    # your code here
+    node = @root
+    return nil if node == nil
+    while true
+      if node.value == value
+        return node
+      elsif node.value > value && node.left != nil
+        node = node.left
+      elsif node.value < value && node.right != nil
+        node = node.right
+      else
+        return nil
+      end
+    end
   end
 
   def insert(value)
-    # your code here
+    return @root = Node.new(value) if @root == nil
+    node = @root
+    while true
+      if value < node.value
+        return node.left = Node.new(value) if node.left == nil
+        node = node.left
+      elsif value > node.value
+        return node.right = Node.new(value) if node.right == nil
+        node = node.right
+      else
+        return node
+      end
+    end
   end
 
 end 
